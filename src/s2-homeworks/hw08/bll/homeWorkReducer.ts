4d5b0142372420c70fdc4b028ff05f3ebe135c69
vladgrudak <ralkavladislav@gmail.com>
2025-06-17 16:55:36 +0300

    Sprint 3 - Homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                return [...state].sort((p1, p2) => p1.name.localeCompare(p2.name))
+            } else if (action.payload === 'down') {
+                return [...state].sort((p1, p2) => p2.name.localeCompare(p1.name))
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            const sortAge = action.payload
+            return state.filter(person => person.age >= sortAge)
         }
         default:
             return state
