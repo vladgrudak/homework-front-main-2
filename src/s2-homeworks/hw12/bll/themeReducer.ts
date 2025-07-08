4d1f437047fd1ab350e22a38f9bf71e9b91f7d1e
vladgrudak <ralkavladislav@gmail.com>
2025-07-08 15:14:45 +0300

    Sprint 3 - Homework 11

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,24 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+export type ThemeState = typeof initState
+
+export const themeReducer = (state = initState, action: ChangeThemeAC): ThemeState => {// fix any
     switch (action.type) {
         // дописать
+        case 'SET_THEME_ID':
+            console.log(action.id)
+            return {themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeAC = {
+    type: 'SET_THEME_ID',
+    id: number
+}
+
+export const changeThemeId = (id: number): ChangeThemeAC => ({type: 'SET_THEME_ID', id}) // fix any
