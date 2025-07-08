b8b1d80adfb37a8d7d5a9bf140417824370295f3
vladgrudak <ralkavladislav@gmail.com>
2025-07-08 12:47:38 +0300

    Sprint 3 - Homework 10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,11 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof initState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {isLoading: action.isLoading}
         default:
             return state
     }
