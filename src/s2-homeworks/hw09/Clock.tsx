efd3ffc52d9c772140159901aa739667bb8eef53
vladgrudak <ralkavladislav@gmail.com>
2025-07-08 12:16:58 +0300

    Sprint 3 - Homework 9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {type MouseEventHandler, useState} from 'react'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import {restoreState} from '../hw06/localStorage/localStorage'
 import s from './Clock.module.css'
@@ -9,30 +9,48 @@ function Clock() {
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
 
-    const start = () => {
+    const start = (event: React.MouseEvent<HTMLButtonElement>) => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
+        const timer = setInterval(() => setDate(new Date), 1000)
+        setTimerId(Number(timer))
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
+        clearInterval(timerId)
+        setTimerId(undefined)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
+        setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
+        setShow(false)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const timeBlock = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+    } as const
+
+    const formatterTimeBlock = new Intl.DateTimeFormat('ru', timeBlock)
+    const formattedTimeBlock = formatterTimeBlock.format(date)
+
+    const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date); // "15"
+    const month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date); // "март"
+    const monthString = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
+    const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date); // "2025"
+    const weekday = new Intl.DateTimeFormat('en', { weekday: 'long' }).format(date); // "понедельник"
+
+
+    const stringTime = formattedTimeBlock || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = `${day}.${month}.${year}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = weekday || <br/> // пишут студенты
+    const stringMonth = monthString || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
@@ -66,14 +84,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={timerId !== undefined} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={timerId === undefined} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
